c282255bcd12b5c475649cc0b99ad6c0c65fedf2
Ian-LS-Cesar <iansantoscesar@gmail.com>
2026-03-25 23:49:46 -0300

    Projetos Spring Boot criados

---
+++ triagem/src/test/java/com/healthsys/triagem/TriagemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthsys.triagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
