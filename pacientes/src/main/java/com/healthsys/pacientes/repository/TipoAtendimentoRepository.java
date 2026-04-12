23681d5eec6ecb4fa849edd49a61071eb81b0025
Ian-LS-Cesar <iansantoscesar@gmail.com>
2026-04-11 22:55:32 -0300

    Atendimento removido (Provisório)

--- pacientes/src/main/java/com/healthsys/pacientes/repository/TipoAtendimentoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.healthsys.pacientes.repository;
-
-import com.healthsys.pacientes.model.TipoAtendimento;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TipoAtendimentoRepository extends JpaRepository<TipoAtendimento, Integer> {
-}
