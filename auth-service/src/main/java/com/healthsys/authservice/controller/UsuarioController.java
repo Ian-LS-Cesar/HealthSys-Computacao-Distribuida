9910414ed46c3755a1d2a00aa8b78ce5ddd54002
Ian-LS-Cesar <iansantoscesar@gmail.com>
2026-04-15 14:36:19 -0300

    CORS para o 5173 funcional

--- auth-service/src/main/java/com/healthsys/authservice/controller/UsuarioController.java
+++ auth-service/src/main/java/com/healthsys/authservice/controller/UsuarioController.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/usuarios")
+@CrossOrigin(origins = "http://localhost:5173")
 public class UsuarioController {
     private final UsuarioService usuarioService;
 
