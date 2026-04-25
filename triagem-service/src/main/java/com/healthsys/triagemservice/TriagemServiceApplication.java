9c28cd6ac197819f8725ff713b46933409044143
Ian-LS-Cesar <iansantoscesar@gmail.com>
2026-04-25 01:15:56 -0300

    Triagem-Service criado

---
+++ triagem-service/src/main/java/com/healthsys/triagemservice/TriagemServiceApplication.java
@@ -0,0 +1,13 @@
+package com.healthsys.triagemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriagemServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TriagemServiceApplication.class, args);
+    }
+
+}
---
+++ triagem-service/src/test/java/com/healthsys/triagemservice/TriagemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthsys.triagemservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriagemServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
