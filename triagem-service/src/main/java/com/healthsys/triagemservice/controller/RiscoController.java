28d4995df78e3adb58a4e608dc5409fbe3712afc
Ian-LS-Cesar <iansantoscesar@gmail.com>
2026-04-25 02:47:11 -0300

    Bug fix

--- triagem-service/src/main/java/com/healthsys/triagemservice/controller/RiscoController.java
+++ triagem-service/src/main/java/com/healthsys/triagemservice/controller/RiscoController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/riscos")
 public class RiscoController {
-    private RiscoService riscoService;
+    private final RiscoService riscoService;
 
     public RiscoController(RiscoService riscoService){
         this.riscoService = riscoService;
