b42952e1960b9cb871c287b673b9f0dafd7cc91f
Ian-LS-Cesar <iansantoscesar@gmail.com>
2026-05-27 00:51:49 -0300

    CORS fix

--- auth-service/src/main/java/com/healthsys/authservice/config/CorsConfig.java
+++ auth-service/src/main/java/com/healthsys/authservice/config/CorsConfig.java
@@ -4,13 +4,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Libera todos os endpoints
-                .allowedOrigins("http://localhost:3000") // URL do seu React
+                .allowedOrigins("http://localhost:5173") // URL do seu React
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*") // Libera todos os headers (inclusive Authorization)
                 .allowCredentials(true);
